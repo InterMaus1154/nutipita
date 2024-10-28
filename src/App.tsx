5e17e0e40c54d930611e29d75f53bd83f19180af
InterMaus1154 <amarco1154@gmail.com>
2024-10-28 00:47:38 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import {FC} from 'react';
 const App: FC = () => {
     return (
         <div className={"app-component"}>
-            <h1>Nincs itt semmi reklám kérem szépen</h1>
-            <h2>🫓🫓🫓🫓🫓🫓🫓🫓🫓🫓🫓🥙🥙🥙🥙🥙🥙🥙🥙🥙🥙🥙🥙🥙</h2>
+            <h1>Nutipita</h1>
         </div>
     );
 };
