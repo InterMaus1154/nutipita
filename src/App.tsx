73d46c23270c080edca78a0e675477bbd4dc65c2
InterMaus1154 <amarco1154@gmail.com>
2024-10-28 01:12:00 +0000

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {FC} from 'react';
+import "App.css";
 
 const App: FC = () => {
     return (
         <div className={"app-component"}>
-            <h1>Nutipita</h1>
+
         </div>
     );
 };
