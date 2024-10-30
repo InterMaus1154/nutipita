ac2c13f98074fc7e2238598c7ea50c890d8ad8e2
InterMaus1154 <amarco1154@gmail.com>
2024-10-30 11:45:09 +0000

    u

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,15 @@
 import {FC} from 'react';
-
+import "./footer.css";
 /**
  * Page footer
  * @constructor
  */
 const Footer: FC = () => {
     return (
-        <footer>
-            NutiPita &copy; {new Date().getFullYear()}
+        <footer className={"full-width"}>
+            <div className="_wrapper">
+                Nuti Pita &copy; {new Date().getFullYear()}
+            </div>
         </footer>
     );
 };
