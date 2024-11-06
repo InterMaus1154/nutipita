f1a38cb67d2fe2d1ff0fdd34bfd26a362086a947
InterMaus1154 <amarco1154@gmail.com>
2024-11-06 00:42:10 +0000

    u

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home: FC = () => {
 
     const {setPageTitle} = useContext(HeaderContext);
 
-    useEffect(()=>{
+    useEffect(() => {
         setPageTitle("Home");
     }, [setPageTitle]);
 
@@ -14,6 +14,22 @@ const Home: FC = () => {
         <section className={"page"}>
             <PageTitle>NutiPita</PageTitle>
             <h2>Welcome to Nuti Pita!</h2>
+            <div>
+                <p>Nutipita is a family-run artisan Pita Bakery established in 2025, specializing in high-quality pita
+                    bread.
+                </p>
+                <p>Founded by Tamas, a seasoned baker with over 30 years of experience, Nutipita proudly supplies
+                    everything
+                    from small supermarkets to large restaurant chains.
+                </p>
+                <p>As a family business, we are committed to maintaining the highest standards, ensuring that every pita
+                    is
+                    crafted with care and precision for an exceptional taste,pocket and texture.
+                </p>
+                <p>Quality is our tradition,
+                    and it’s what sets us apart.
+                </p>
+            </div>
         </section>
     );
 };
