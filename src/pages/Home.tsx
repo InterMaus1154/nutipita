38bc9f5efa126ae1ace551f31c8c6ce39bb8d912
InterMaus1154 <amarco1154@gmail.com>
2024-11-06 00:36:44 +0000

    u

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
-import {FC} from 'react';
+import {FC, useContext, useEffect} from 'react';
 import PageTitle from "../components/PageTitle.tsx";
+import {HeaderContext} from "../context/HeaderContext.tsx";
 
 const Home: FC = () => {
+
+    const {setPageTitle} = useContext(HeaderContext);
+
+    useEffect(()=>{
+        setPageTitle("Home");
+    }, [setPageTitle]);
+
     return (
         <section className={"page"}>
             <PageTitle>NutiPita</PageTitle>
