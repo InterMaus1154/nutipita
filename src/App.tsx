e9d43d3a4e6cc348f0600b9dbf8b5f59cdbdd5a4
InterMaus1154 <amarco1154@gmail.com>
2024-11-10 19:52:29 +0000

    scrolling router issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import {FC} from 'react';
+import {FC, ReactNode, useLayoutEffect} from 'react';
 import "./App.css";
 import Header from "./components/header/Header.tsx";
 import Footer from "./components/footer/Footer.tsx";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, useLocation} from "react-router-dom";
 import Home from "./pages/home/Home.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import Contact from "./pages/contact/Contact.tsx";
 import {faArrowUp} from "@fortawesome/free-solid-svg-icons/faArrowUp";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-const ScrollToButton: FC = () => {
 
+const Wrapper: FC<{ children: ReactNode }> = ({children}) => {
+
+    const location = useLocation();
+
+    useLayoutEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "instant"
+        });
+    }, [location.pathname]);
+
+    return children;
+}
+
+const ScrollToButton: FC = () => {
 
     const scrollToTop = () => {
         window.scrollTo({
@@ -21,7 +36,7 @@ const ScrollToButton: FC = () => {
 
     return (
         <button aria-label={"Go back to top"} onClick={scrollToTop} className={"scroll-top-button"}>
-            <FontAwesomeIcon icon={faArrowUp} />
+            <FontAwesomeIcon icon={faArrowUp}/>
         </button>
     );
 }
@@ -33,14 +48,16 @@ const App: FC = () => {
         <div className={"app-component"}>
             <Header/>
             <main>
-                <Routes>
-                    <Route path={"*"} element={<NotFound/>}/>
-                    <Route index path={"/"} element={<Home/>}/>
-                    <Route path={"/contact"} element={<Contact/>}/>
-                </Routes>
+                <Wrapper>
+                    <Routes>
+                        <Route path={"*"} element={<NotFound/>}/>
+                        <Route index path={"/"} element={<Home/>}/>
+                        <Route path={"/contact"} element={<Contact/>}/>
+                    </Routes>
+                </Wrapper>
             </main>
             <Footer/>
-            <ScrollToButton />
+            <ScrollToButton/>
         </div>
     );
 };
