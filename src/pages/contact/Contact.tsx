bd69374448b9742512e9c2df9e2232919e59db40
InterMaus1154 <amarco1154@gmail.com>
2024-12-02 11:57:35 +0000

    accessibility issues fix

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -11,42 +11,42 @@ const Contact: FC = () => {
                 <div className="contact-wrapper">
                     <AnimatedBox className="contact-box">
                         <h3 className="contact-box-header">Mobile</h3>
-                        <a href={"tel:+447754226325"} className="icon-wrapper">
+                        <a href={"tel:+447754226325"} className="icon-wrapper" aria-label={"Phone number: +447754226325 "}>
                             <img src="icon/phoneicon.png" alt="Phone icon" aria-hidden={true}/>
                         </a>
                         <a href="tel:+447754226325" className="contact-info">+447754226325</a>
                     </AnimatedBox>
                     <AnimatedBox className="contact-box">
                         <h3 className="contact-box-header">Email</h3>
-                        <a href={"mailto:nutipita@gmail.com"} className="icon-wrapper email-icon">
+                        <a href={"mailto:nutipita@gmail.com"} className="icon-wrapper email-icon" aria-label={"Email address: nutipita@gmail.com"}>
                             <img src={"icon/email.png"} alt={"Email icon"} aria-hidden={true}/>
                         </a>
                         <a className={"contact-info"} href="mailto:nutipita@gmail.com">nutipita@gmail.com</a>
                     </AnimatedBox>
                     <AnimatedBox className="contact-box">
                         <h3 className="contact-box-header">WhatsApp</h3>
-                        <a href={"https://wa.me/447754226325"} className="icon-wrapper">
+                        <a href={"https://wa.me/447754226325"} className="icon-wrapper" aria-label={"Whatsapp number: +447754226325"}>
                             <img src="icon/whatsappicon.png" alt="Whatsapp icon" aria-hidden={true}/>
                         </a>
                         <a href="https://wa.me/447754226325" className="contact-info">+447754226325</a>
                     </AnimatedBox>
                     <AnimatedBox className="contact-box">
                         <h3 className="contact-box-header">Instagram</h3>
-                        <a className="icon-wrapper">
+                        <a className="icon-wrapper" href={"#"} aria-label={"Instagram"}>
                             <img src="icon/instaicon.png" alt="Instagram icon" aria-hidden={true}/>
                         </a>
                         <a href="" className={"contact-info"}>@nutipita</a>
                     </AnimatedBox>
                     <AnimatedBox className="contact-box">
                         <h3 className="contact-box-header">TikTok</h3>
-                        <a className="icon-wrapper">
+                        <a className="icon-wrapper" href={"#"} aria-label={"Tiktok"}>
                             <img src="icon/tiktokicon.png" alt="Tiktok icon" aria-hidden={true}/>
                         </a>
                         <a href="" className={"contact-info"}>@nutipita</a>
                     </AnimatedBox>
                     <AnimatedBox className="contact-box">
                         <h3 className={"contact-box-header"}>Location</h3>
-                        <a href="https://maps.app.goo.gl/dHEy2pm6o8jzjoc79" className={"icon-wrapper"} >
+                        <a href="https://maps.app.goo.gl/dHEy2pm6o8jzjoc79" className={"icon-wrapper"} aria-label={"Location of our bakery"}>
                             <img src="icon/mapicon.png" alt="Map pin icon" aria-hidden={true}/>
                         </a>
                         <a href="https://maps.app.goo.gl/dHEy2pm6o8jzjoc79"
@@ -62,6 +62,7 @@ const Contact: FC = () => {
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2477.762984332091!2d-0.0717926231477124!3d51.60923127183671!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761e9818999969%3A0xd47e022c32f85e4a!2sLanghedge%20Industrial%20Estate%2C%20Langhedge%20Ln%2C%20London%20N18%202TQ!5e0!3m2!1sen!2suk!4v1730372582475!5m2!1sen!2suk"
                         allowFullScreen={true} loading="lazy"
+                        title={"Google map of the location"}
                         referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </AnimatedBox>
             </article>
