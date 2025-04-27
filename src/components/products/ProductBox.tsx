dfa5eb31c17616aa65132cbe6098c1ede1c3d728
intermaus1154 <amarco1154@gmail.com>
2025-04-27 21:46:14 +0200

    Left right image switching

--- src/components/products/ProductBox.tsx
+++ src/components/products/ProductBox.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from 'react';
+import {FC, useEffect, useState} from 'react';
 import ProductDetails from "./ProductDetails.ts";
 
 interface Props {
@@ -8,14 +8,27 @@ interface Props {
 const ProductBox: FC<Props> = ({productData}) => {
 
     const [images] = useState<string[]>(productData.image.images);
-    const [currentImage, setCurrentImage] = useState<string>(images[0]);
+    const [index, setIndex] = useState<number>(0);
+    const [currentImage, setCurrentImage] = useState<string>(images[index]);
+
+    const rightClickHandler = () =>{
+        setIndex(prevState => (prevState + 1) % images.length);
+    };
+
+    const leftClickHandler = () =>{
+        setIndex(prevState => (prevState - 1 + images.length) % images.length);
+    };
+
+    useEffect(() => {
+        setCurrentImage(images[index]);
+    }, [index, images, setCurrentImage]);
 
     return (
         <div className={"product-box"}>
             <div className="product-box-header">
                 <img src={productData.image.folder + currentImage} alt=""/>
-                <button className={"image-selector-button left"}>&larr;</button>
-                <button className={"image-selector-button right"}>&rarr;</button>
+                <button className={"image-selector-button left"} onClick={leftClickHandler}>&larr;</button>
+                <button className={"image-selector-button right"} onClick={rightClickHandler}>&rarr;</button>
             </div>
             <div className="product-box-body">
                 <h3>{productData.title}</h3>
