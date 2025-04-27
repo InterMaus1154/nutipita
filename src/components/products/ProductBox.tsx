9f359d0491ac1a5cd490c959c201a045d38e34cb
intermaus1154 <amarco1154@gmail.com>
2025-04-27 21:31:21 +0200

    Add image skipper buttons

--- src/components/products/ProductBox.tsx
+++ src/components/products/ProductBox.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
 import ProductDetails from "./ProductDetails.ts";
 
 interface Props {
@@ -6,11 +6,21 @@ interface Props {
 }
 
 const ProductBox: FC<Props> = ({productData}) => {
+
+    const [images] = useState<string[]>(productData.image.images);
+    const [currentImage, setCurrentImage] = useState<string>(images[0]);
+
     return (
         <div className={"product-box"}>
-            <img src={productData.image.folder + productData.image.images[0]} alt=""/>
-            <h3>{productData.title}</h3>
-            <h4>{productData.subtitle}</h4>
+            <div className="product-box-header">
+                <img src={productData.image.folder + currentImage} alt=""/>
+                <button className={"image-selector-button left"}>&larr;</button>
+                <button className={"image-selector-button right"}>&rarr;</button>
+            </div>
+            <div className="product-box-body">
+                <h3>{productData.title}</h3>
+                <h4>{productData.subtitle}</h4>
+            </div>
         </div>
     );
 };
