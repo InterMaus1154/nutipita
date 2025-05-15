954f65b3d21d633a96522982f1e07884f51c5808
intermaus1154 <amarco1154@gmail.com>
2025-05-15 09:30:22 +0100

    Add image viewer control buttons

--- src/components/image_viewer/ImageViewer.tsx
+++ src/components/image_viewer/ImageViewer.tsx
@@ -11,16 +11,29 @@ interface Props {
 const ImageViewer: FC<Props> = ({visible, setVisible, images, currentImageIndex}) => {
 
     const [selectedImage, setSelectedImage] = useState<string>(images[currentImageIndex]);
+    const [selectedImageIndex, setSelectedImageIndex] = useState<number>(currentImageIndex);
+
+    const onPreviousImageHandler = () => {
+        setSelectedImageIndex(prevState => ((prevState - 1) + images.length) % images.length);
+    };
+
+    const onNextImageHandler = () => {
+        setSelectedImageIndex(prevState => (prevState + 1) % images.length);
+    };
 
     useEffect(() => {
-        setSelectedImage(images[currentImageIndex]);
-    }, [currentImageIndex, images]);
+        setSelectedImageIndex(currentImageIndex);
+    }, [currentImageIndex]);
+
+    useEffect(() => {
+        setSelectedImage(images[selectedImageIndex]);
+    }, [selectedImageIndex, images, setSelectedImage]);
 
     useEffect(() => {
         // Disable body scroll
-        if(visible){
+        if (visible) {
             document.body.classList.add("no-scroll");
-        }else{
+        } else {
             document.body.classList.remove("no-scroll");
         }
 
@@ -33,21 +46,24 @@ const ImageViewer: FC<Props> = ({visible, setVisible, images, currentImageIndex}
     return (
         visible &&
         <div className={"image-viewer-overlay"}>
-            <button className={"close-image-viewer-button"} onClick={()=> {setVisible(false)}}>Close</button>
+            <button className={"close-image-viewer-button"} onClick={() => {
+                setVisible(false)
+            }}>Close
+            </button>
             <div className="image-viewer">
                 <div className="selected-image-wrapper">
                     <img src={selectedImage} alt=""/>
                 </div>
-                <div className="image-carousel">
-                    {images.map((image: string, index: number) => {
-                        return(
-                            <div className={"image-carousel-wrapper"}>
-                                <img src={image} alt=""/>
-                            </div>
-                        );
-                    })}
+                <div className="control-buttons">
+                    {/*show previous image*/}
+                    <button onClick={onPreviousImageHandler} aria-label={"Show previous image"}
+                            title={"Show previous image"}>&lt;</button>
+                    {/*show next image*/}
+                    <button onClick={onNextImageHandler} aria-label={"Show next image"}
+                            title={"Show next image"}>&gt;</button>
                 </div>
             </div>
+
         </div>
     );
 };
